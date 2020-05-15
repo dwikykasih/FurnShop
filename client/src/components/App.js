0143b1631470f4c818e0248c74b92c42fa309600
muhammadjulian <muhammad.14116165@student.itera.ac.id>
2020-05-15 23:15:41 +0700

    update tampilan login register

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,13 @@
 import React, { Suspense } from 'react';
 import { Route, Switch } from "react-router-dom";
 import Auth from "../hoc/auth";
+// pages for this product
 import Homepage from "./views/HomePage/Homepage.js";
 import NavBar from "./views/NavBar/NavBar";
 import Footer from "./views/Footer/Footer";
+import Login from "./views/Activity/Login.js";
+import Register from "./views/Activity/Register.js";
+
 
 function App() {
   return (
@@ -12,10 +16,11 @@ function App() {
       <div style={{ paddingTop: '75px', minHeight: 'calc(100vh - 80px)' }}>
         <Switch>
           <Route exact path="/" component={Auth(Homepage, null)} />
-          //isi link nya disini jul
+          <Route exact path="/login" component={Auth(Login, false)} />
+          <Route exact path="/register" component={Auth(Register, false)} />
         </Switch>
       </div>
-      <Footer/>
+      <Footer />
     </Suspense>
   );
 }
--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
 const config = require("./config/key");
+const config = require("./config/dev");
 
 // const mongoose = require("mongoose");
 // mongoose
